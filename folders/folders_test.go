bdabf1159cc9163df5a855662d444e6f28bf1ba6
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-10-17 13:34:42 +1100

    first assert test working: req is not nil!

--- folders/folders_test.go
+++ folders/folders_test.go
@@ -2,12 +2,17 @@ package folders_test
 
 import (
 	"testing"
-	// "github.com/georgechieng-sc/interns-2022/folders"
-	// "github.com/stretchr/testify/assert"
+	"github.com/georgechieng-sc/interns-2022/folders"
+	"github.com/stretchr/testify/assert"
+	"github.com/gofrs/uuid"
 )
 
 func Test_GetAllFolders(t *testing.T) {
-	t.Run("test", func(t *testing.T) {
-		// your test/s here
-	})
+	req := &folders.FetchFolderRequest{
+		OrgID: uuid.FromStringOrNil(folders.DefaultOrgID),
+	}
+	assert.NotNil(t, req)
+	// t.Run("test", func(t *testing.T) {
+	// 	assert.NotNil(folders.GetAllFolders(req))
+	// })
 }
