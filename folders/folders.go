8c9611fc98ac1f703e6f8424444ed8dedee11bf2
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-10-17 13:02:24 +1100

    added comments around what I think the code does and some suggestions for refactoring

--- folders/folders.go
+++ folders/folders.go
@@ -4,34 +4,35 @@ import (
 	"github.com/gofrs/uuid"
 )
 
-func GetAllFolders(req *FetchFolderRequest) (*FetchFolderResponse, error) {
-	// var (
+func GetAllFolders(req *FetchFolderRequest) (*FetchFolderResponse, error) { // called by main and gets passed the OrgID via FFR type
+	// var ( // TODO use or discard these unuser variables
 	// 	err error
 	// 	f1  Folder
 	// 	fs  []*Folder
 	// )
-	f := []Folder{}
-	r, _ := FetchAllFoldersByOrgID(req.OrgID)
-	for _, v := range r {
-		f = append(f, *v)
+	f := []Folder{} // an instance of the folder struct 
+	r, _ := FetchAllFoldersByOrgID(req.OrgID) // calling the func, passing the OrgID and storing the output
+	for _, v := range r { // for all the key val pairs in r
+		f = append(f, *v) // put a pointer to each val in f
 	}
-	var fp []*Folder
-	for _, v1 := range f {
-		fp = append(fp, &v1)
+	var fp []*Folder // init pointer folder
+	for _, v1 := range f { // for each key val pair in f
+		fp = append(fp, &v1) // append the address of val to fp
 	}
-	var ffr *FetchFolderResponse // TODO merge down to one line using := 
-	ffr = &FetchFolderResponse{Folders: fp}
-	return ffr, nil
+	var ffr *FetchFolderResponse // TODO merge down to one line using := // ffr is assigned to the pointer of FetchFolderResponse
+	ffr = &FetchFolderResponse{Folders: fp} // then it's assigned to the address of FetchFolderResponse with something that looks similar to object destructuring
+	return ffr, nil // return the object containing the list of folders 
 }
 
-func FetchAllFoldersByOrgID(orgID uuid.UUID) ([]*Folder, error) {
-	folders := GetSampleData()
+func FetchAllFoldersByOrgID(orgID uuid.UUID) ([]*Folder, error) { // takes the orgID
+	folders := GetSampleData() // calling the function that reads "sample.json" storing the results in a var
 
-	resFolder := []*Folder{}
-	for _, folder := range folders {
-		if folder.OrgId == orgID {
-			resFolder = append(resFolder, folder)
-		}
+	resFolder := []*Folder{} // init a folder with pointer
+	for _, folder := range folders { // map over the sample data and check to see if the orgID matches the one we passed to this function
+		if folder.OrgId == orgID { 
+			resFolder = append(resFolder, folder) // append those that match to resFolder
+		} // TODO test functionality when there are varied OrgId's might be a good idea
 	}
-	return resFolder, nil
+	
+	return resFolder, nil // return resFolder // resFolder == sample.json as all OrgId are the same 
 }
